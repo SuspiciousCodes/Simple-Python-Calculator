c19b9836efaab96f2f520114f714ac4ce759bf50
Suspicious <117949144+SuspiciousCodes@users.noreply.github.com>
2024-10-01 21:23:29 +0100

    Implement basic calculator functionality
    Add basic calculator functionality with operations: addition, subtraction, multiplication, and division

---
+++ Calculator Program.py
@@ -0,0 +1,36 @@
+# Simple Python Calculator
+# Author: @SuspiciousCodes on Github.
+# Date: 01/10/2024
+# Version : v1.0
+
+operator = input("Enter an operator (+ - * /): ")
+print(" ")
+
+num1 =  float(input("Enter the 1st number: "))
+
+num2 =  float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(" ")
+    print (f"Your answer is: {round(result, 2)}") 
+    
+elif operator == "-":
+    result = num1 - num2
+    print(" ")
+    print (f"Your answer is: {round(result, 2)}")    
+    
+elif operator == "*":
+    result = num1 * num2
+    print(" ")
+    print (f"Your answer is: {round(result, 2)}")    
+    
+elif operator == "/":
+    result = num1 / num2
+    print(" ")
+    print (f"Your answer is: {round(result, 2)}")
+        
+else:
+    print(f"'{operator}' is not a valid operator!")
+
+    
